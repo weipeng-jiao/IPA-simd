4fc657bfdbd451e3ccd2ac1ca662f575f2cc741c
weipeng-jiao <58947917+weipeng-jiao@users.noreply.github.com>
2022-08-15 01:00:53 +0800

    add adsolute value for array

---
+++ demo.c
@@ -0,0 +1,20 @@
+/* The absolute value algorithm for input array
+@ auther: Kepler Jiao
+@ time  : 2022/08/14
+*/
+
+
+//The absolute value algorithm for input array using c language
+void Calc_Abs_C(signed char *array, unsigned int len)
+{
+    for(int i=0 ; i<len ; i++ )
+    {
+        if(value[i]<0)
+        {
+            value[i]=-value[i];
+        }
+    }
+}
+
+//The absolute value algorithm for input array using hvx instrctions
+void Calc_Abs_Hvx(sig)
